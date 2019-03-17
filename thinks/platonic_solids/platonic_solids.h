54409351bd4fdcf73850d90248f7dacd354ceb9c
Tommy Hinks <tommy.hinks@raysearchlabs.com>
2019-03-17 21:03:10 +0100

    first commit of meshes in header files

--- thinks/platonic_solids/platonic_solids.h
+++ thinks/platonic_solids/platonic_solids.h
@@ -10,22 +10,48 @@
 #include <vector>
 
 namespace thinks {
+namespace platonic_solids_internal {
+
+template <typename VerticesT, typename IndicesT>
+constexpr std::pair<VerticesT, IndicesT> MakeMesh(
+    const VerticesT &vertices, const IndicesT &indices) noexcept {
+  static_assert(std::is_floating_point_v<typename VerticesT::value_type>,
+                "FloatT must be a floating point type");
+  static_assert(std::is_integral_v<typename IndicesT::value_type>,
+                "IndexT must be an integral type");
+  return std::make_pair(vertices, indices);
+}
+
+} // platonic_solids_internal
+
+template <typename PositionT, std::size_t VertexCount, 
+          typename IndexT, std::size_t TriangleCount>
+struct TriangleMesh {
+  std::array<PositionT, 3 * VertexCount> positions;
+  std::array<IndexT, 3 * TriangleCount> triangle_indices;
+
+  static_assert(std::is_floating_point_v<PositionT>,
+                "FloatT must be a floating point type");
+  static_assert(std::is_integral_v<IndexT>, 
+                "IndexT must be an integral type");
+};
 
 template <typename FloatT, typename IndexT>
-constexpr std::pair<std::array<FloatT, 6 * 3>, std::array<IndexT, 8 * 3>> 
+using OctahedronTriangleMesh = TriangleMesh<FloatT, 6, IndexT, 8>;
+
+template <typename FloatT, typename IndexT>
+constexpr OctahedronTriangleMesh<FloatT, IndexT>
 Octahedron() noexcept {
-  static_assert(std::is_floating_point_v<FloatT>, "FloatT must be a floating point type");
-  static_assert(std::is_integral_v<IndexT>, "IndexT must be an integral type");
-  return std::make_pair(
-    std::array<FloatT, 6 * 3>{{
+  return OctahedronTriangleMesh<FloatT, IndexT>{
+    decltype(OctahedronTriangleMesh<FloatT, IndexT>::positions){{
       0, 0, -1,
       -1, 0, 0,
       0, 1, 0,
       1, 0, 0,
       0, -1, 0,
       0, 0, 1
-    }}, 
-    std::array<IndexT, 8 * 3>{{
+    }},
+    decltype(OctahedronTriangleMesh<FloatT, IndexT>::triangle_indices){{
       0, 1, 2,
       3, 0, 2,
       1, 0, 4,
@@ -34,7 +60,8 @@ Octahedron() noexcept {
       4, 0, 3,
       5, 1, 4,
       5, 4, 3      
-    }});
+    }}        
+  };
 }
 
 } // namespace thinks
